8c45f59eba57c374b4e862576b7942a0b910ffd4
Spider <pawan.sharma@emails.emizentech.com>
2024-07-18 16:33:27 +0530

    Magento2 homeicon

---
+++ Block/HomeIcon.php
@@ -0,0 +1,12 @@
+<?php
+namespace Techmail\Homeicon\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class HomeIcon extends Template
+{
+    protected function _prepareLayout()
+    {
+        return parent::_prepareLayout();
+    }
+}
